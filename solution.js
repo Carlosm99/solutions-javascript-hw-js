a84e95db2d2f78efec26a416f74a22657a118b38
carlos monterrosa <carlosmontess07@gmail.com>
2020-10-10 02:16:25 -0500

    firts solution

---
+++ solution.js
@@ -0,0 +1,36 @@
+var tabla = ///from the table that I will create   
+function maketable() {
+    // vincula al cuerpo de html
+    var div1 = document.getElementById('div1');
+
+    // crear las tablas
+    var tbl = document.createElement("table");
+ 
+    // creating filas
+    for (var r = 0; r < tabla; r++) {
+        var row = document.createElement("tr");
+     
+     // celdas adentro de las filas
+         for (var c = 0; c < tabla; c++) {
+              var cell = document.createElement("td");
+   
+            if (r === c){
+            var cellText = document.createTextNode([r][c]="1");
+            
+            cell.appendChild(cellText);
+            }         
+            if (r != c){
+            var cellText = document.createTextNode([r][c]="0");
+            
+            cell.appendChild(cellText);
+            } 
+           row.appendChild(cell);
+           tbl.appendChild(row);      //suma lo que se vaya agregando
+        }      
+        
+        
+    }
+    
+    div1.appendChild(tbl); // tabla adentro del div
+}
+window.onload=drawTable; 
